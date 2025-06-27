4b1ff7b2245c2d03b8bc5aa2cdbad8c918ed14ef
Md Omor <mashrufahmed03@gmail.com>
2025-06-27 18:06:14 +0600

    some changes

--- src/routes/recipe.route.js
+++ src/routes/recipe.route.js
@@ -20,7 +20,7 @@ recipeRoute.route('/update-recipes/:id').put(recipeUpdateById);
 recipeRoute.route('/like/:id').patch(toggleLike);
 recipeRoute.route('/delete-recipes/:id').delete(recipeDeleteById);
 recipeRoute.route('/my-recipes/:id').get(myRecipes);
-recipeRoute.get('/recipes/summary/:userId').get(getRecipeSummaryByUser);
-recipeRoute.get('/recipes/:id/view').get(incrementRecipeView);
+recipeRoute.route('/recipes/summary/:userId').get(getRecipeSummaryByUser);
+recipeRoute.route('/recipes/:id/view').get(incrementRecipeView);
 
 export default recipeRoute;
